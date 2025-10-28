5c039af2d66b7471ed95a38fe382898ad9ea1ed6
jaydxxp <jaydeepwaghaskar2005@gmail.com>
2025-10-28 17:57:47 +0530

    added a blog

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,9 +8,12 @@ export function cn(...inputs: ClassValue[]) {
 export function formatDate(date: string) {
   let currentDate = new Date().getTime();
   if (!date.includes("T")) {
-    date = `${date}T00:00:00`;
+    date = `${date}T00:00:00`
+    ;
   }
+  
   let targetDate = new Date(date).getTime();
+    if (isNaN(targetDate)) return "Invalid Date";
   let timeDifference = Math.abs(currentDate - targetDate);
   let daysAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
 
